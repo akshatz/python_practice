fe1bc421aedfe5ba7958ff11f7433b2003189d40
Akshat Zala <akshat.zala@cuelogic.com>
2019-08-22 11:40:23 +0530

    added docstring

--- Assignment/question_7.py
+++ Assignment/question_7.py
@@ -15,7 +15,16 @@
 # ABd1234@1
 # Hints:
 # In case of input data being supplied to the question, it should be assumed to be a console input.
-
+"""
+	A program to validate password
+	Following are the criteria for checking the password:
+	1. At least 1 letter between [a-z]
+	2. At least 1 number between [0-9]
+	3 At least 1 letter between [A-Z]
+	4. At least 1 character from [$#@]
+	5. Minimum length of transaction password: 6
+	6. Maximum length of transaction password: 12
+"""
 import re
 password = input("Enter passwords separated by comma: ").split(",")
 
