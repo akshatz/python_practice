9133d5bf84bc405633c3a6b67df5ea492b5f62d1
Akshat Zala <50409080+akshatz123@users.noreply.github.com>
2019-08-22 11:43:13 +0530

    added docstring

---
+++ python_practice/placeholder.py
@@ -0,0 +1,9 @@
+# name = 'marcog'
+# number = 42
+# print ('%s %d' % (name, number))
+# for i in range(0,11):
+# 	print(i*5)
+def add(x, y):
+	return (x + y)
+sum = add(4, 5)
+print(sum)
---
+++ python_practice/third.py
@@ -0,0 +1,14 @@
+"""
+	List
+"""
+
+def students():
+	student =["RSA", "AES", "DES"]
+	print(student)
+	return (student);
+
+students()
+name = "Akshat"
+
+sent = "%s is %d years old"
+sent%("Akshat", 11)
